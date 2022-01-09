0840de5c61acc79c3ecb142358aa58b24510be7a
Jimmy Ritter <jimmy.ritter@redacreltd.com>
2022-01-09 17:27:24 +0100

    fix on server port

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,19 +6,19 @@ import ExchangeRoute from "./routes";
 // configuring dotenv to access private API key
 dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const server = express();
+server.use(cors());
+server.use(express.json());
 
 // configure services routes
-app.use('/exchange', ExchangeRoute);
+server.use('/exchange', ExchangeRoute);
 
 // backend should not be initiated without Exchange Rates API key properly defined
 if (!process.env.EXCHANGE_RATES_API_KEY) {
   throw Error("Exchange Rates API Key must be specified within the environment file.");
 }
 
-const port = 3001;
-app.listen(port, () => {
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
